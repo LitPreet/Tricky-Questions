03ae1a264617c4c51128d5d0ce21c2cce0029199
Preet bhardwaj <77205201+LitPreet@users.noreply.github.com>
2023-03-21 22:23:19 +0530

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,12 @@
+// C++ code that prints all natural numbers up to N without the use of a semicolon.  
+#include<iostream>  
+using namespace std;  
+  
+int N = 15;  
+int main ()  
+{  
+static int a = 9;  
+if ( cout << a << " " && a++ < N && main () )  
+{ }  
+return 0;  
+}  
